958f85971d3a83c406c39964a583b44fc5dfa313
hs6074 <hs6074@srmist.edu.in>
2026-03-02 09:05:50 +0530

    Updated UC1 and UC2 - Banner OOPS

--- UC1OOPS.java
+++ UC1OOPS.java
@@ -1,4 +1,4 @@
-public class OopsConsole{
+public class UC1OOPS{
 	public static void main(String args[]){
 		System.out.println("OOPS");
 	}
--- UC2OOPS.java
+++ UC2OOPS.java
@@ -1,4 +1,4 @@
-public class BannerConsole {
+public class UC2OOPS {
 	public static void main(String[] args) {
 		System.out.println(" *****   *****   *****   ***** ");
 		System.out.println("*     * *     * *     * *     *");
