46f76d683c6ed9e8137cbfadaf1e91bbfbcd2fe0
hs6074 <hs6074@srmist.edu.in>
2026-03-03 08:17:10 +0530

    Banner Oops

---
+++ UC06OOPS.java
@@ -0,0 +1,43 @@
+public class UC06OOPS {
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+    static String[] getP() {
+        return new String[]{
+                "****** ",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      "
+        };
+    }
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
+    }
+    public static void main(String[] args) {
+        String[][] banner = {
+                getO(),
+                getO(),
+                getP(),
+                getS()
+        };
+        for (int row = 0; row < banner[0].length; row++) {
+            for (int col = 0; col < banner.length; col++) {
+                System.out.print(banner[col][row] + "  ");
+            }
+            System.out.println();
+        }
+    }
+}
